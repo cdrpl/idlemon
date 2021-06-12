38b6a892d60caa1964c3e39f96f9d1f0d4001ff9
cdrpl <cdrpl@gmail.com>
2021-06-12 16:08:35 +0800

    convert to errors.Is()

--- unit_template_test.go
+++ unit_template_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"database/sql"
+	"errors"
 	"testing"
 
 	. "github.com/cdrpl/idlemon-server"
@@ -40,7 +41,7 @@ func TestRandUnitTemplateID(t *testing.T) {
 		var scan int
 		err = db.QueryRow("SELECT id FROM unit_template WHERE id = ?", id).Scan(&scan)
 		if err != nil {
-			if err == sql.ErrNoRows {
+			if errors.Is(err, sql.ErrNoRows) {
 				t.Errorf("received an invalid ID: %v", id)
 			} else {
 				t.Fatalf("query error: %v", err)
